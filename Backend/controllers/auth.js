99ae071abde7d4029459c58a7090c058a4e265b0
iftikarjahan <iftikarjahan22@gmail.com>
2024-10-15 16:28:03 +0530

    add login feature using a jwt token

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -14,7 +15,7 @@ exports.signup = (req, res, next) => {
   const name = req.body.name;
   const password = req.body.password;
   bcrypt
-    .hash(password,12)
+    .hash(password, 12)
     .then((hashedPassword) => {
       const user = new User({
         email: email,
@@ -27,6 +28,41 @@ exports.signup = (req, res, next) => {
       res
         .status(201)
         .json({ message: "User Created😅😅🫡", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No user with this email is found🥶🥶");
+        error.statusCode = 401;
+        throw err;
+      }
+      loadedUser=user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("User enetered a wrong Password😶‍🌫️😶‍🌫️");
+        error.statusCode = 401;
+        throw err;
+      }
+      const jwtPayload={
+        email:loadedUser.email,
+        userId:loadedUser._id.toString()
+      }
+      const token=jwt.sign(jwtPayload,"secretKey",{expiresIn:"1hr"});
+      res.status(200).json({token:token,userId:loadedUser._id.toString()});
     })
     .catch((err) => {
       if (!err.statusCode) {
